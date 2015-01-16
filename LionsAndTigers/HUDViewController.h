296215d4c2b720ac5dc6b5e6dbcc973a413ed422
EvanCV <evan.c.vandenberg@gmail.com>
2015-01-15 16:26:01 -0800

    first commit

---
+++ LionsAndTigers/HUDViewController.h
@@ -0,0 +1,13 @@
+//
+//  HUDViewController.h
+//  LionsAndTigers
+//
+//  Created by Evan Vandenberg on 1/15/15.
+//  Copyright (c) 2015 Evan Vandenberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HUDViewController : UIViewController
+
+@end
---
+++ LionsAndTigers/TopViewController.h
@@ -0,0 +1,13 @@
+//
+//  TopViewController.h
+//  LionsAndTigers
+//
+//  Created by Evan Vandenberg on 1/15/15.
+//  Copyright (c) 2015 Evan Vandenberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TopViewController : UIViewController
+
+@end
